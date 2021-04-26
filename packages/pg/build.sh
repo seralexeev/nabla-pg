e9fbe08a02fe72f314e276d741514add04985747
Sergey Alexeev <ser.alexeev@gmail.com>
2021-04-27 08:01:31 +1000

    fix bin

--- packages/pg/build.sh
+++ packages/pg/build.sh
@@ -6,8 +6,7 @@ rm -rf dist
 tsc -p tsconfig.build.json
 tsc-alias
 cp package.json dist/
-cp -r bin dist/
+cp cli dist/
 
 mv dist/pg/index.js dist/pg/index.d.ts ./dist
 sed -i '' 's!./src!./pg/src!g' dist/index.js dist/index.d.ts
-# sed -i '' 's!./src!./pg/src!g'
