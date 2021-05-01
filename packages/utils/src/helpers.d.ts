da853b677900c29be57c257baf4d08bd164eb129
Sergey Alexeev <ser.alexeev@gmail.com>
2021-05-01 15:45:27 +1000

    build script

--- packages/utils/src/helpers.d.ts
+++
@@ -1,3 +0,0 @@
-import { FilterKeys } from '@flstk/utils/types';
-export declare const pick: <T, K extends keyof T>(value: T, keys: readonly K[]) => Pick<T, K>;
-export declare const reduceBy: <T, R = T>(arr: T[], keySelector: FilterKeys<T, string | number> | ((t: T, index: number) => string), map?: (t: T, index: number) => R) => Record<string, R>;
--- packages/utils/src/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { pick, reduceBy } from '@flstk/utils/helpers';
-export { ArrayElement, FilterKeys, InverseFilterKeys, NominalType, NotNullableKeys, NullableKeys, TypeTag, UnwrapNominal, UnwrapNominalTag, } from '@flstk/utils/types';
